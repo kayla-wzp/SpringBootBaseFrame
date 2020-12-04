1e5d83a84a7ea90833c796e5c353d36e66a460d3
kayla-wzp <411930867@qq.com>
2020-12-04 14:35:34 +0800

    one:create project

---
+++ frame-template/src/main/java/frame/template/FrameTemplateApplication.java
@@ -0,0 +1,13 @@
+package frame.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameTemplateApplication.class, args);
+    }
+
+}
